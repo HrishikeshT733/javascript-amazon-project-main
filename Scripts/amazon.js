530f3b2ff5fb34cb0c3a2e420a6c86094b045b16
Hrishikesh <hrishikeshtholbare@gmail.com>
2025-07-22 16:10:16 +0530

    Make the Cart Quantity Interactive

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -78,7 +78,13 @@ let matchingItem;
     quantity:1
     });
     }
- 
-    console.log(cart);
+
+    let cartQuantity=0;
+    cart.forEach((item)=>{
+     cartQuantity+=item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+   
 });
 });
