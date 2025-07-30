6b6bf5b50c78dd0a1bafdd8d56e5ae5c808ceadd
Hrishikesh <hrishikeshtholbare@gmail.com>
2025-07-30 20:08:04 +0530

    Save Cart in LocalStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,6 @@
-export let cart=[
+export let cart=JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+    cart=[
 {
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'
     ,quantity:2
@@ -9,6 +11,12 @@ export let cart=[
 
 ];
 
+}
+
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addtoCart(productId){
 let matchingItem;
@@ -28,6 +36,7 @@ let matchingItem;
     quantity:1
     });
     }
+    saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -40,4 +49,5 @@ if(cartItem.productId!==productId){
 
 });
 cart =newCart;
+saveToStorage();
 }
