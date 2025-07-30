46ca5b71706be6bd72d938dbdaa889e7f7ba4755
Hrishikesh <hrishikeshtholbare@gmail.com>
2025-07-30 20:00:07 +0530

    Deletes products from HTML when clicking Delete

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -13,7 +13,7 @@ if(product.id===productId){
 });
 
 cartSummaryHTML+=`
-<div class="cart-item-container">
+<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Delivery date: Tuesday, June 21
             </div>
@@ -99,7 +99,11 @@ document.querySelectorAll('.js-delete-link')
 link.addEventListener('click',()=>{
 const productId=link.dataset.productId;
 removeFromCart(productId);
-console.log(cart);
+const container=document.querySelector(
+    `.js-cart-item-container-${productId}`
+    );
+  container.remove();
+
 });
 
 })
