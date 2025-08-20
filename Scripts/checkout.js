91bbf6aa903e0e8cb0ebd089fcf747b1b19ba63d
Hrishikesh <hrishikeshtholbare@gmail.com>
2025-08-20 20:49:42 +0530

    Convert Products into classes

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentService.js";
-import '../data/cart-class.js';
+//import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
